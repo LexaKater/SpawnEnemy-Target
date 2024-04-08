6e492a5aee1a374a4bdc39ca7c41afc35c30aae6
LexaKater <125663812+LexaKater@users.noreply.github.com>
2024-04-08 15:44:09 +0300

    Add files via upload

---
+++ Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _waypoints = new List<Transform>();
+
+    private float _speed = 2f;
+    private int _currentWaypoint = 0;
+
+    private void Update() => Move();
+
+    private void Move()
+    {
+        if (transform.position == _waypoints[_currentWaypoint].position)
+            _currentWaypoint = ++_currentWaypoint % _waypoints.Count;
+
+        transform.position = Vector3.MoveTowards(transform.position, _waypoints[_currentWaypoint].position, _speed * Time.deltaTime);
+    }
+}
